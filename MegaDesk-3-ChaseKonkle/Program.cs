6e4ca8b512733d5e147dae692646fe647c26795c
Chase Konkle <ckonkle@byui.edu>
2017-12-05 07:36:02 -0600

    Finished MegaDesk 1.0

--- MegaDesk-3-ChaseKonkle/Program.cs
+++ MegaDesk-3-ChaseKonkle/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MegaDesk_3_ChaseKonkle
@@ -16,7 +13,7 @@ namespace MegaDesk_3_ChaseKonkle
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainMenu());
         }
     }
 }
